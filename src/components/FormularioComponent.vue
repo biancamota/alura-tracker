765128ed1a9ef9d7dcad5dde51551741aeda7c26
biafranca <bianca.franca@bling.com.br>
2023-10-03 17:46:25 -0300

    feat: define componentes

---
+++ src/components/FormularioComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="column is-8" role="form" aria-label="Formulaŕio de cadastro de tarefas">
+                <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar ?">
+            </div>
+            <div class="column is-4">
+                <div class="is-flex is-align-items-center is-justify-content-space-between">
+                    <section>
+                        <strong>00:00:00</strong>
+                    </section>
+                    <button class="button">
+                        <span class="icon">s
+                            <i class="fas fa-play"></i>
+                        </span>
+                        <span>play</span>
+                    </button>
+                    <button class="button">
+                        <span class="icon">
+                            <i class="fas fa-stop"></i>
+                        </span>
+                        <span>stop</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'FormularioComponent'
+})
+</script>
+
+<style scoped>
+
+</style>
