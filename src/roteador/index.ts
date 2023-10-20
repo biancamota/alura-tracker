295c0b01471ab379bca9824b9df81fc0f6cce611
Bianca Mota <biancamota1209@gmail.com>
2023-10-20 00:11:12 -0300

    feat: completa crud de projetos

--- src/roteador/index.ts
+++ src/roteador/index.ts
@@ -1,23 +1,36 @@
-import { RouteRecordRaw, createRouter, createWebHashHistory } from "vue-router";
-import Tarefa from "../views/Tarefa.vue";
-import Projeto from "../views/Projeto.vue";
+import IProjeto from "@/interfaces/IProjeto";
+import { createStore, Store, useStore as vuexUseStore } from "vuex";
+import { InjectionKey } from 'vue'
+import { ADICIONA_PROJETO, ALTERA_PROJETO, EXCLUIR_PROJETO } from "../store/tipo-mutacoes";
 
-const rotas: RouteRecordRaw[] = [
-    {
-        path: '/',
-        name: 'Tarefas',
-        component: Tarefa
+interface Estado {
+    projetos: IProjeto[]
+}
+
+export const key: InjectionKey<Store<Estado>> = Symbol()
+
+export const store = createStore<Estado>({
+    state: {
+        projetos: []
     },
-    {
-        path: '/projetos',
-        name: 'Projetos',
-        component: Projeto
+    mutations: {
+        [ADICIONA_PROJETO](state, nomeDoProjeto: string) {
+            const projeto = {
+                id: new Date().toISOString(),
+                nome: nomeDoProjeto
+            } as IProjeto
+            state.projetos.push(projeto)
+        },
+        [ALTERA_PROJETO] (state, projeto: IProjeto) {
+            const index = state.projetos.findIndex(proj => proj.id == projeto.id)
+            state.projetos[index] = projeto
+        },
+        [EXCLUIR_PROJETO](state, id: string) {
+            state.projetos = state.projetos.filter(proj => proj.id != id)
+        }
     }
-];
-
-const roteador = createRouter({
-    history: createWebHashHistory(),
-    routes: rotas
 })
 
-export default roteador;
+export function useStore(): Store<Estado> {
+    return vuexUseStore(key)
+}
